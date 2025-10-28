c0bd5af8eac8c086e4fe7894df65819924646576
Sanket <mailsanketagrawal@gmail.com>
2025-10-28 23:58:18 +0530

    website routes

--- src/api/v1/website/website.controller.ts
+++ src/api/v1/website/website.controller.ts
@@ -25,6 +25,7 @@ export const fetchAllCategories = async (req : Request, res : Response) => {
 
 export const fetchTherapistProfiles = async (req : Request, res : Response) => {
     try {
+        const {categoryId , subCategoryId} = req.query
         const therapistProfiles = await fetchTherapistProfileService();
         res.status(200).json(
             new ApiResponse(true,200,"Therapist profiles fetched successfully",therapistProfiles)

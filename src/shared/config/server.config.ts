6deeae1c339cd6a210cb3925b4c447585699f287
Sanket <mailsanketagrawal@gmail.com>
2025-12-07 02:31:48 +0530

    changes in frontend urls

--- src/shared/config/server.config.ts
+++ src/shared/config/server.config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const serverConfig = {
   port: process.env.PORT || 4000,
   nodeEnv: process.env.NODE_ENV || 'development',
-  baseFrontendUrl : "https://www.catalystcare.in",
+  baseFrontendUrl : process.env.FRONTEND_BASE_URL as string,
   superAdminEmail : process.env.SUPER_ADMIN_EMAIL as string,
 };
 

94deb832dd5ec41a03e36cb8d1ae0ff851a99c49
Sanket <mailsanketagrawal@gmail.com>
2026-01-30 00:18:54 +0530

    added promethus monitoring

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import './infrastructure/redis/index'
 import bullMqRouter from "./infrastructure/bullMq/index";
 import { registerRepeatableJobs } from "./infrastructure/jobs/daily-jobs";
 import connectMongoDB from "./infrastructure/mongodb/index";
+import { register } from "./infrastructure/monitoring/metrics";
+import { httpMetricsMiddleware } from "./infrastructure/monitoring/http-metrics";
 
 dotenv.config();
 
@@ -16,6 +18,13 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet()); // security headers
 app.use(morgan("combined")); // logging
+
+app.use(httpMetricsMiddleware);
+
+app.get("/metrics", async (_req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 
 app.use('/api',routes);
 
